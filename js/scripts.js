18903ed3ac8e3e40f0377dd62506b233ae6a7498
Jacque <ayojackay@gmail.com>
2017-10-02 14:56:50 -0700

    finished. want to revamp soon

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 // target your button and assign an ask function that outputs a random image to your page
-var questions = ["Are you awesome?", "Will that wish come true?", "Do you know what you're doing?"];
-var imagePic = ["magic8ball_1.png", "magic8ball_2.png", "magic8ball_3.png"];
-var diffQuestion = [Math.floor(Math.random() * questions.length)];
-var diffPic = [Math.floor(Math.random() * imagePic.length)];
+// let questions = ["Are you awesome?", "Will that wish come true?", "Do you know what you're doing?"];
+// console.log(questions);
+//
+// function magicQuestions() {
+//     let quest = Math.floor(Math.random() * questions.length);
+//   document.getElementById("magic-question").innerHTML = questions[quest];
+//   }
+// console.log(magicQuestions());
 
-console.log(diffPic);
-  function magicAnswer() {
-      document.getElementById("eightball").getElementsByTagName("img").src = imagePic[diffPic];
+//adding image to randomize
+let imagePic = ['img/magic8ball_1.png', 'img/magic8ball_2.png', 'img/magic8ball_3.png', 'img/magic8ball_4.png', 'img/magic8ball_5.png', 'img/magic8ball_6.png', 'img/magic8ball_7.png', 'img/magic8ball_8.png', 'img/magic8ball_9.png', 'img/magic8ball_10.png',
+ 'img/magic8ball_11.png', 'img/magic8ball_12.png', 'img/magic8ball_13.png', 'img/magic8ball_14.png', 'img/magic8ball_15.png', 'img/magic8ball_16.png', 'img/magic8ball_17.png', 'img/magic8ball_18.png', 'img/magic8ball_19.png', 'img/magic8ball_20.png'];
+console.log(imagePic);
+
+  function magicAnswer(button) {
+    let diffPic = Math.floor(Math.random() * imagePic.length);
+      document.getElementById("eightball").src= imagePic[diffPic];
   }
+
 console.log(magicAnswer());
-
-function magicQuestions() {
-    var yo = document.getElementById("magic--question");
-    yo.innerHTML = questions[diffQuestion];
-  }
-  console.log(magicQuestions);
